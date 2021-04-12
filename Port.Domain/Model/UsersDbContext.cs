705f19842ff418ec5d6ddd0f8ce1054c04ea8b10
muzieh <muzieh@gmail.com>
2021-04-12 22:15:34 +0200

    Kick off

---
+++ Port.Domain/Model/UsersDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Port.Domain.Model
+{
+    public class UsersDbContext : DbContext
+    {
+       public DbSet<Student> Students { get; set; }
+       public DbSet<Course> Courses { get; set; }
+
+       public UsersDbContext()
+       {
+           this.Database.EnsureCreated();
+       }
+       
+       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       {
+           optionsBuilder
+               .UseNpgsql("Host=localhost;Username=postgres;Password=example;Database=Users", builder => builder
+                   .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery))
+               .EnableDetailedErrors();
+           
+       }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+       {
+           modelBuilder.Entity<Student>(x =>
+           {
+               x.HasKey(u => u.Id);
+               x.Property(p => p.Id).HasColumnName("StudentId");
+               x.Property(p => p.Email);
+               x.Property(p => p.FirstName);
+               x.Property(p => p.LastName);
+               x.HasOne(p => p.FavoriteCourse).WithMany();
+           });
+
+           modelBuilder.Entity<Course>(x =>
+           {
+               x.HasKey(u => u.Id);
+               x.Property(p => p.Id).HasColumnName("CourseId");
+               x.Property(p => p.Name);
+           });
+
+       }
+    }
+}
---
+++ Port.Domain/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Port.Domain.Model;
+
+namespace Port.Domain
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await using var context = new UsersDbContext();
+            var studentFaker = new Faker<Student>()
+                .CustomInstantiator(f => new Student(f.Name.FirstName() ,f.Name.LastName(), f.Internet.Email(), new Course("course 1")));
+            
+            /*
+            Enumerable.Range(0,100).ToList().ForEach( _ =>
+            {
+                context.Students.Add(studentFaker.Generate());
+                context.SaveChanges();
+            });
+            */
+
+
+            var students = await context.Students.Include(p => p.FavoriteCourse).ToListAsync();
+            
+            foreach (var student in students)
+            {
+               Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
